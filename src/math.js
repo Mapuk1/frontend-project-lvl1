da3fe46b70b25cada3fc0822c6893c8902d8eda1
Марат Вальшин <mar-valshin@yandex.ru>
2022-01-26 04:41:37 +0400

    add math.js

---
+++ src/math.js
@@ -0,0 +1,7 @@
+const getRandomIntInclusive = (min, max) => {
+  const num1 = Math.ceil(min);
+  const num2 = Math.floor(max);
+  return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+};
+
+export default getRandomIntInclusive;
